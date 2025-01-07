b9b48b0436c398ec7f39055b2b169f1e77806739
Julia-wr <julia.wr59@web.de>
2025-01-07 14:06:39 +0100

    barrel

--- geometry/prog_mesh.gd
+++ geometry/prog_mesh.gd
@@ -1,158 +1,83 @@
-@tool
-
 extends MeshInstance3D
 
-@export_range(3, 720) var segments = 6:
-	set(value):
-		segments = value
-		_recreate_mesh()
-	get:
-		return segments
-
-@export var height := 2:
-	set(value):
-		height = value
-		_recreate_mesh()
-	get:
-		return height
-
-@export var radius := 1:
-	set(value):
-		radius = value
-		_recreate_mesh()
-	get:
-		return radius
+@export var radius := 5
+@export var rings := 25
+@export var radial_segments := 10
+@export var barrel_width := 0.9
 
 
-func _enter_tree() :
-	_recreate_mesh()
+func _ready():
+	var verts := PackedVector3Array()
+	var uvs := PackedVector2Array()
+	var normals := PackedVector3Array()
+	var indices := PackedInt32Array()
 	
-func _recreate_mesh():
-	mesh = ArrayMesh.new()
-
-	#The Cap
-	var surface_array_cap = []
-	surface_array_cap.resize(Mesh.ARRAY_MAX)
+	var cur_row := 0
+	var last_row := 0
+	var point := 0
 	
-	var verts_cap = PackedVector3Array()
-	var norms_cap = PackedVector3Array()
-	var index_cap = PackedInt32Array()
-
-	var alpha = 2*PI / segments
-	
-	verts_cap.push_back(Vector3(radius, height/2, 0))
-	norms_cap.push_back(Vector3(0, 1, 0))
-	
-	for n in range(1, segments):
-		var x = radius * cos(n * alpha)
-		var z = radius * sin(n * alpha)
+	for i in range(rings + 1):
+		var v := float(i) / rings
+		var w := sin(PI * v)
+		var y := cos(PI * v)
 		
-		verts_cap.push_back(Vector3(x, height/2, z))
-		norms_cap.push_back(Vector3(0, 1, 0))
+		for j in range(radial_segments + 1):
+			var u := float(j) / radial_segments
+			
+			# Times 2.0 so it can paste it.
+			var x := radius * sin(u * PI * 2.0)
+			var z := radius * cos(u * PI * 2.0)
+			
+			var vertices := Vector3(
+				x * radius * w * barrel_width,
+				y * radius,
+				z * radius * w * barrel_width
+			)
+			if i <= rings / 5 or i >= rings - rings / 5:
+				vertices.z = sin(PI * radius)
+				vertices.x = sin(PI * radius)
+				vertices.y = 0
+				normals.append(Vector3(0, 1 if i <= rings / 5 else -1, 0))
+			else:
+				normals.append(vertices.normalized())
+			verts.append(vertices)
+			
+			uvs.append(Vector2(u, v))
+			point += 1
+
+			if i > 0 and j > 0:
+				indices.append(last_row + j - 1)
+				indices.append(last_row + j)
+				indices.append(cur_row + j - 1)
+				indices.append(last_row + j)
+				indices.append(cur_row + j)
+				indices.append(cur_row + j - 1)
 		
-		index_cap.push_back(n-1)
-		index_cap.push_back(n)
-		index_cap.push_back(segments)
-
-	verts_cap.push_back(Vector3(0, 0 ,0))
-	norms_cap.push_back(Vector3(0, 1, 0))
-
-	index_cap.push_back(segments-1)
-	index_cap.push_back(0)
-	index_cap.push_back(segments)
+		last_row = cur_row
+		cur_row = point
 	
-	surface_array_cap[Mesh.ARRAY_VERTEX] = verts_cap
-	surface_array_cap[Mesh.ARRAY_NORMAL] = norms_cap
-	surface_array_cap[Mesh.ARRAY_INDEX]  = index_cap
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array_cap)
+	var material := StandardMaterial3D.new()
+	material.albedo_color = Color.BROWN
 	
-	#the bottom
-	var surface_array_bot = []
-	surface_array_bot.resize(Mesh.ARRAY_MAX)
+	var arr_mesh = ArrayMesh.new()
+	var arrays := []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = verts
+	arrays[Mesh.ARRAY_TEX_UV] = uvs
+	arrays[Mesh.ARRAY_NORMAL] = normals
+	arrays[Mesh.ARRAY_INDEX] = indices
 	
-	var verts_bot = PackedVector3Array()
-	var norms_bot = PackedVector3Array()
-	var index_bot = PackedInt32Array()
-	
-	verts_bot.push_back(Vector3(radius, -height/2, 0))
-	norms_bot.push_back(Vector3(0, -1, 0))
-	
-	for n in range(1, segments):
-		var x = radius * cos(n * alpha)
-		var z = radius * sin(n * alpha)
-		
-		verts_bot.push_back(Vector3(x, -height/2, z))
-		norms_bot.push_back(Vector3(0, -1, 0))
-		
-		index_bot.push_back(n)
-		index_bot.push_back(n-1)
-		index_bot.push_back(segments)
-
-	verts_bot.push_back(Vector3(0, -height / 2 ,0))
-	norms_bot.push_back(Vector3(0, -1, 0))
-
-	index_bot.push_back(0)
-	index_bot.push_back(segments-1)
-	index_bot.push_back(segments)
-	
-	surface_array_bot[Mesh.ARRAY_VERTEX] = verts_bot
-	surface_array_bot[Mesh.ARRAY_NORMAL] = norms_bot
-	surface_array_bot[Mesh.ARRAY_INDEX]  = index_bot
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array_bot)
-	
-	#sides
-	var surface_array_sides = []
-	surface_array_sides.resize(Mesh.ARRAY_MAX)
-	var verts_sides = PackedVector3Array()
-	var norms_sides = PackedVector3Array()
-	var index_sides = PackedInt32Array()
-	
-	#cap on x axis
-	verts_sides.push_back(Vector3(radius, height/2, 0))
-	norms_sides.push_back(Vector3(1, 0, 0))
-	#bottom on x axis
-	verts_sides.push_back(Vector3(radius, -height/2, 0))
-	norms_sides.push_back(Vector3(1, 0, 0))
-	
-	for n in range(1, segments):
-		var x = cos(n * alpha)
-		var z = sin(n * alpha)
-		#current cap vertex
-		verts_sides.push_back(Vector3(radius * x, height/2, radius * z))
-		norms_sides.push_back(Vector3(x, 0, z))
-		
-		#current bottom vertex
-		verts_sides.push_back(Vector3(radius * x, -height/2, radius * z))
-		norms_sides.push_back(Vector3(x, 0, z))
-		
-		#first triangle
-		index_sides.push_back(2 * n + 1)
-		index_sides.push_back(2 * n)
-		index_sides.push_back(2 * (n - 1))
-		#second triangle
-		index_sides.push_back(2 * n + 1)
-		index_sides.push_back(2 * (n - 1))
-		index_sides.push_back(2 * (n - 1) + 1)
-		
-	
-	verts_sides.push_back(Vector3(0, -height / 2 ,0))
-	norms_sides.push_back(Vector3(0, -1, 0))
-
-	index_sides.push_back(2 * 0 + 1)
-	index_sides.push_back(2 * 0)
-	index_sides.push_back(2 * (segments - 1))
-	#second triangle
-	index_sides.push_back(2 * 0 + 1)
-	index_sides.push_back(2 * (segments - 1))
-	index_sides.push_back(2 * (segments - 1) + 1)
-	
-	surface_array_sides[Mesh.ARRAY_VERTEX] = verts_sides
-	surface_array_sides[Mesh.ARRAY_NORMAL] = norms_sides
-	surface_array_sides[Mesh.ARRAY_INDEX]  = index_sides
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array_sides)
-	
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	arr_mesh.surface_set_material(0, material)
+	mesh = arr_mesh
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_left"):
+		rotation.y += PI * delta
+	if Input.is_action_pressed("ui_right"):
+		rotation.y -= PI * delta
+	if Input.is_action_pressed("ui_up"):
+		rotation.x += sin(PI * delta)
+	if Input.is_action_pressed("ui_down"):
+		rotation.x -= sin(PI * delta)
